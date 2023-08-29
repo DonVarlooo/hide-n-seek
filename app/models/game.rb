3c0dc6e368fc2d614aced6e67fae709f9d4951d2
Adrien Regis <adrien.regis@gmail.com>
2023-08-29 16:51:17 +0200

    push

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,13 @@
 class Game < ApplicationRecord
   after_validation :set_radius
   validates :duration, presence: true
-  reverse_geocoded_by :lat, :lng
   before_save :geocode, if: :will_save_change_to_lng?
+  reverse_geocoded_by :lat, :lng
 
+  # geocoded_by :lat
+  # geocoded_by :lng
+  # after_validation :geocode, if: :will_save_change_to_lat?
+  # after_validation :geocode, if: :will_save_change_to_lng?
   private
 
   def set_radius
